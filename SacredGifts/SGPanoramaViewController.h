9f2922c5725bfc56b021d0d621807d648811c86d
Ontario Britton <ontario.britton@gmail.com>
2013-10-10 12:23:47 -0400

    Music confirmed playing in Aalborg and Gethsemane.

---
+++ SacredGifts/SGPanoramaViewController.h
@@ -0,0 +1,14 @@
+//
+//  SGPanoramaViewController.h
+//  SacredGifts
+//
+//  Created by Ontario on 10/10/13.
+//  Copyright (c) 2013 Wells Fargo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface SGPanoramaViewController : GLKViewController
+
+@end
---
+++ SacredGifts/SGPanoramaViewController.m
@@ -0,0 +1,13 @@
+//
+//  SGPanoramaViewController.m
+//  SacredGifts
+//
+//  Created by Ontario on 10/10/13.
+//  Copyright (c) 2013 Wells Fargo. All rights reserved.
+//
+
+#import "SGPanoramaViewController.h"
+
+@implementation SGPanoramaViewController
+
+@end
