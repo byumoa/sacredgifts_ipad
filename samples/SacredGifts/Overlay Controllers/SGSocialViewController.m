ea79065bb2e4d8218b708539d6422947b2436f34
Ontario Britton <ontario.britton@gmail.com>
2013-11-07 22:26:50 -0700

    App Store social autofill added.

--- samples/SacredGifts/Overlay Controllers/SGSocialViewController.m
+++ samples/SacredGifts/Overlay Controllers/SGSocialViewController.m
@@ -9,8 +9,22 @@
 #import "SGSocialViewController.h"
 #import <Social/Social.h>
 
-NSString* const kShareInitialText = @"Enjoying Sacred Gifts at the MOA";
+NSString* const kEmailAutofill = @"Enjoying the painting by %@ while using the Sacred Gifts app from the BYU Museum of Art. %@";
+NSString* const kTwitterAutofill = @"Viewing this painting while using the @BYUmoa’s #sacredgifts app.";
+NSString* const kFacebookAutofill = @"Viewing this painting by %@ and feeling grateful while using the #sacredgifts app from the BYU Museum of Art.";
+
+NSString* const kAppStoreURL = @"http://appstore.com/sacredgifts";
 int const kOverlayHeight = 236;
+
+@interface SGSocialViewController()
+{
+    NSArray* _schwartzPaintings;
+    NSArray* _hofmannPaintings;
+}
+
+- (NSString*)calcArtistForPaintingStr: (NSString*)paintingName;
+
+@end
 
 @implementation SGSocialViewController
 
@@ -19,6 +33,9 @@ int const kOverlayHeight = 236;
     if( self = [super initWithCoder:aDecoder]){
         _centerPos = CGPointMake(384, 720);
         self.moduleType = kModuleTypeSocial;
+        
+        _schwartzPaintings = @[@"garden", @"aalborg"];
+        _hofmannPaintings = @[@"capture", @"temple", @"ruler", @"gethsemane", @"savior"];
     }
     
     return self;
@@ -32,32 +49,34 @@ int const kOverlayHeight = 236;
     {
         case SocialMediaTypeFacebook:
         {
-            //if( [SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook])
-            //{
-                SLComposeViewController *socialSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
-                [socialSheet setInitialText:(NSString*)kShareInitialText];
-                [socialSheet addImage:thumbnail];
-                [self presentViewController:socialSheet animated:YES completion:^{}];
-            //}
+            NSString* artist = [self calcArtistForPaintingStr:self.paintingName];
+            NSString* autofillStr = [NSString stringWithFormat:kFacebookAutofill, artist];
+            
+            SLComposeViewController *socialSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+            [socialSheet setInitialText:autofillStr];
+            [socialSheet addImage:thumbnail];
+            [socialSheet addURL:[NSURL URLWithString:kAppStoreURL]];
+            [self presentViewController:socialSheet animated:YES completion:^{}];
         }
             break;
         case SocialMediaTypeTwitter:
         {
-            //if( [SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
-            //{
-                SLComposeViewController *socialSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-                [socialSheet setInitialText:(NSString*)kShareInitialText];
-                [socialSheet addImage:thumbnail];
-                [self presentViewController:socialSheet animated:YES completion:^{}];
-            //}
+            SLComposeViewController *socialSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+            [socialSheet setInitialText:(NSString*)kTwitterAutofill];
+            [socialSheet addImage:thumbnail];
+            [socialSheet addURL:[NSURL URLWithString:kAppStoreURL]];
+            [self presentViewController:socialSheet animated:YES completion:^{}];
         }
             break;
         case SocialMediaTypeEmail:
         {
+            NSString* artist = [self calcArtistForPaintingStr:self.paintingName];
+            NSString* autofillStr = [NSString stringWithFormat:kEmailAutofill, artist, kAppStoreURL];
+            
             MFMailComposeViewController *mailController = [[MFMailComposeViewController alloc] init];
             [mailController setMailComposeDelegate:self];
             [mailController setSubject:@"Sacred Gifts"];
-            [mailController setMessageBody:kShareInitialText isHTML:NO];
+            [mailController setMessageBody:autofillStr isHTML:NO];
             [self presentViewController:mailController animated:YES completion:^{}];
             NSData *myData = UIImageJPEGRepresentation(thumbnail, 1.0);
             [mailController addAttachmentData:myData mimeType:@"image/png" fileName:@"Thumbnail.png"];
@@ -66,6 +85,20 @@ int const kOverlayHeight = 236;
         default:
             break;
     }
+}
+
+-(NSString *)calcArtistForPaintingStr:(NSString *)paintingName
+{
+    for (NSString* pName in _schwartzPaintings ) {
+        if( [pName isEqualToString:paintingName])
+            return @"Frans Schwartz";
+    }
+    for (NSString* pName in _hofmannPaintings ) {
+        if( [pName isEqualToString:paintingName])
+            return @"Heinrich Hofmann";
+    }
+    
+    return @"Carl Bloch";
 }
 
 -(void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
