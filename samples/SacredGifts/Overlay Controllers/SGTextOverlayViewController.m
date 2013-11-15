2c80f5984bf12a0439fae29091edfe593fe2a6e3
Ontario Britton <ontario.britton@gmail.com>
2013-11-15 11:13:09 -0700

    Garden Norrensby text+image close button positioned lower like pano button

--- samples/SacredGifts/Overlay Controllers/SGTextOverlayViewController.m
+++ samples/SacredGifts/Overlay Controllers/SGTextOverlayViewController.m
@@ -34,6 +34,10 @@ const CGRect kTextFrame = {0, 730, 768, 183};
 {
     [super addBackgroundImgWithPath:bgImgPath];
     //self.view.frame = kTextFrame;
+    NSLog(@"self.closeButton: %@", self.closeButton);
+    CGPoint center = self.closeButton.center;
+    center.y = self.view.frame.size.height - 32;
+    self.closeButton.center = center;
 }
 
 @end
