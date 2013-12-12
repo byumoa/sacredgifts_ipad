53325b373980b83767f437a410f6cf36d1f102aa
Ontario Britton <ontario.britton@gmail.com>
2013-12-12 10:33:30 -0700

    Sermon AR begun

---
+++ samples/SacredGifts/Overlay Controllers/SGSermonShapesOverlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGSermonShapesOverlayViewController.h
+//  SacredGifts
+//
+//  Created by Ontario on 12/12/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGSermonShapesOverlayViewController : UIViewController
+
+@end
